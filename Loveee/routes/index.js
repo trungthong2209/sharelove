a5f871271cdf584d0dfc5329b9406cc36f945e21
Thong <doanthong002@gmail.com>
2020-11-30 12:50:11 +0700

    update map
    fix map.ejs

--- Loveee/routes/index.js
+++ Loveee/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var createRoom = require("../../../../Loveee/updatecode_cap1/sharelove/Loveee/controller/Room")
 
 router.get('/setting',function(req, res, next) {
    res.render('setting');
